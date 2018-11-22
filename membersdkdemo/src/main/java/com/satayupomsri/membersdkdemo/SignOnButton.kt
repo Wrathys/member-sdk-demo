95894a36a4ed4d88df381fb56ef311291905d704
satayu <satayu132@gmail.com>
2018-11-22 15:20:50 +0700

    disable proguard

--- membersdkdemo/src/main/java/com/satayupomsri/membersdkdemo/SignOnButton.kt
+++ membersdkdemo/src/main/java/com/satayupomsri/membersdkdemo/SignOnButton.kt
@@ -173,6 +173,30 @@ class SignOnButton : FrameLayout, View.OnClickListener, Dialog.OnListener {
         this.onSignInListenerSuccess(id, name, avatar)
     }
 
+    interface OnSignInListener {
+        /**
+         * Callback when sing in success
+         *
+         * @param id user id.
+         * @param name user name.
+         */
+        fun onSignInSuccess(id: String, name: String, avatar: String)
+
+        /**
+         * Callback when sing in fail
+         *
+         * @param status status code response.
+         */
+        fun onSignInFail(status: String)
+
+        /**
+         * Callback when sing out
+         *
+         * @param status status code response.
+         */
+        fun onSignOut(status: String)
+    }
+
     fun setOnSigninListener(listener: OnSignInListener) {
         this.onSignInListener = listener
 
