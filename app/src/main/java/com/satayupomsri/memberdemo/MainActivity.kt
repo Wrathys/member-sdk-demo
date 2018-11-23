42c7acef5bdcad70f886de177603df120706f3ac
satayu <satayu132@gmail.com>
2018-11-23 14:43:14 +0700

    fix invalid directory resource and disable main project

--- app/src/main/java/com/satayupomsri/memberdemo/MainActivity.kt
+++ app/src/main/java/com/satayupomsri/memberdemo/MainActivity.kt
@@ -8,7 +8,7 @@ import android.view.View
 import com.bumptech.glide.Glide
 import com.satayupomsri.memberdemo.signinsdk.ListenerSignInSdk
 import com.satayupomsri.memberdemo.signinsdk.utils.Prefs
-import com.satayupomsri.membersdkdemo.utils.MemberData
+//import com.satayupomsri.membersdkdemo.utils.MemberData
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,71 +20,71 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Listen to provide the data that request by sdk sign in
-        ListenerSignInSdk(this)
-
-        pref = Prefs(this)
-
-        if (pref!!.memberData.isSignIn()) {
-            val (id, name, avatar) = pref!!.memberData.getMemberData()
-            tv_member_id.text = id
-            tv_member_name.text = name
-            Glide.with(this)
-                    .load(avatar)
-                    .into(iv_member_avatar)
-            tv_already_account.visibility = View.INVISIBLE
-        } else {
-            setDefaultUi()
-        }
-
-        initButtonSignSignOut()
+//        ListenerSignInSdk(this)
+//
+//        pref = Prefs(this)
+//
+//        if (pref!!.memberData.isSignIn()) {
+//            val (id, name, avatar) = pref!!.memberData.getMemberData()
+//            tv_member_id.text = id
+//            tv_member_name.text = name
+//            Glide.with(this)
+//                    .load(avatar)
+//                    .into(iv_member_avatar)
+//            tv_already_account.visibility = View.INVISIBLE
+//        } else {
+//            setDefaultUi()
+//        }
+//
+//        initButtonSignSignOut()
     }
 
     private fun initButtonSignSignOut() {
-        bt_go_sign_in.apply {
-            text = if (pref!!.memberData.isSignIn())
-                resources.getString(R.string.bt_sign_out)
-            else
-                resources.getString(R.string.bt_sign_in)
-
-            setOnClickListener { _ ->
-                run {
-                    if (pref!!.memberData.isSignIn()) {
-                        pref!!.memberData = MemberData("", "", "", false)
-                        setDefaultUi()
-                    } else {
-                        this@MainActivity.startActivityForResult(Intent(context, SignInActivity::class.java), 1)
-                    }
-                }
-            }
-        }
+//        bt_go_sign_in.apply {
+//            text = if (pref!!.memberData.isSignIn())
+//                resources.getString(R.string.bt_sign_out)
+//            else
+//                resources.getString(R.string.bt_sign_in)
+//
+//            setOnClickListener { _ ->
+//                run {
+//                    if (pref!!.memberData.isSignIn()) {
+////                        pref!!.memberData = MemberData("", "", "", false)
+//                        setDefaultUi()
+//                    } else {
+//                        this@MainActivity.startActivityForResult(Intent(context, SignInActivity::class.java), 1)
+//                    }
+//                }
+//            }
+//        }
     }
 
     private fun setDefaultUi() {
-        tv_member_id.text = ""
-        tv_member_name.text = ""
-        tv_member_status.text = ""
-        iv_member_avatar.setImageDrawable(getDrawable(R.drawable.ic_account_circle_black_24dp))
-
-        tv_already_account.visibility = View.VISIBLE
-        bt_go_sign_in.text = resources.getString(R.string.bt_sign_in)
+//        tv_member_id.text = ""
+//        tv_member_name.text = ""
+//        tv_member_status.text = ""
+//        iv_member_avatar.setImageDrawable(getDrawable(R.drawable.ic_account_circle_black_24dp))
+//
+//        tv_already_account.visibility = View.VISIBLE
+//        bt_go_sign_in.text = resources.getString(R.string.bt_sign_in)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == 1) {
-            if (resultCode == Activity.RESULT_OK) {
-                tv_member_id.text = data?.getStringExtra(getString(R.string.member_id_key))
-                tv_member_name.text = data?.getStringExtra(getString(R.string.member_name_key))
-                Glide.with(this)
-                        .load(data?.getStringExtra(getString(R.string.member_avatar_key)))
-                        .into(iv_member_avatar)
-                tv_already_account.visibility = View.INVISIBLE
-                initButtonSignSignOut()
-            }
-            if (resultCode == Activity.RESULT_CANCELED) {
-                //Write your code if there's no result
-            }
-        }
+//        if (requestCode == 1) {
+//            if (resultCode == Activity.RESULT_OK) {
+//                tv_member_id.text = data?.getStringExtra(getString(R.string.member_id_key))
+//                tv_member_name.text = data?.getStringExtra(getString(R.string.member_name_key))
+//                Glide.with(this)
+//                        .load(data?.getStringExtra(getString(R.string.member_avatar_key)))
+//                        .into(iv_member_avatar)
+//                tv_already_account.visibility = View.INVISIBLE
+//                initButtonSignSignOut()
+//            }
+//            if (resultCode == Activity.RESULT_CANCELED) {
+//                //Write your code if there's no result
+//            }
+//        }
     }
 }
